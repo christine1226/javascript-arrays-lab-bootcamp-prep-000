d2e5fb717b4a550403ed77504ad8e9a3cce48715
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-08-19 15:03:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ function destructivelyAppendKitten(name){
   kittens.push(name);
   return kittens;
 }
+
+function destructivelyPrependKitten(name){
+  
+}
+
+function detructivelyRemoveLastKitten(){
+  kitten.pop();
+  return kitten;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kitten.shift()
+return kitten;
+}
+
+function appendKitten(name){
+  var newArray = [... "kitten", "name"];
+  return newArray;
+}
+
+function prependKitten(name){
+  var newArray= ["name"... "kittens"];
+  return newArray;
+}
+
+function removeLastKitten(){
+  var newArray= kittens.slice[2,1,"name"];
+  return newArray;
+}
+
+function removeFirstKitten(){
+  var newArray= kittens.slice[0,1,"name"]
+}
